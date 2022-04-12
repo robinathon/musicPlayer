351721ce03be007490376292ddf42137ae8d3c43
Robin Kumar <82315764+robinathon@users.noreply.github.com>
2022-04-12 09:40:30 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,14 +3,15 @@ import { v4 as uuidv4 } from "uuid";
 function ChillHop() {
   return [
     {
-      name: "The Next Episode",
-      artist: "Snoop Dogg, Dr. Dre",
+      name: "Jaan Nisaar",
+      artist: "Arijit Singh",
       cover:
-        "https://images.genius.com/7d7c1a4a2f10c54b35605ee8046f503c.1000x1000x1.jpg",
+        "https://i0.wp.com/www.lyricsupgrade.com/wp-content/uploads/2020/07/Jaan-Nisaar-Lyrics-Arijit-Singh.jpg?w=480&ssl=1",
       id: uuidv4(),
       active: true,
       color: ["#642F27", "#9CB4CE"],
-      audio:"https://www.mboxdrive.com/Dr.%20Dre,%20Ft%20Snoop%20Dogg-The%20Next%20Episode%20(lyrics).mp3",
+      audio:"https://uploadbox.io/b13o3b3238b7
+",
     },
 
     {
@@ -21,18 +22,7 @@ function ChillHop() {
       id: uuidv4(),
       active: false,
       color: ["#642F27", "#9CB4CE"],
-      audio:"https://www.mboxdrive.com/The%20Script%20-%20Hall%20Of%20Fame%20(Lyrics).mp3"
-    },
-    
-    {
-      name: "Benanza(Belly Dancer)",
-      artist: "Akon",
-      cover:
-        "https://t2.genius.com/unsafe/409x409/https%3A%2F%2Fimages.genius.com%2Fd4b0a94edc41b877037dc0b3a3143389.316x316x1.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#642F27", "#9CB4CE"],
-      audio:"https://www.mboxdrive.com/Y2Mate.is%20-%20Bananza%20(Belly%20Dancer)-FobOaKDli28-160k-1638699023254.mp3"
+      audio:"https://uploadbox.io/sgik5ey8wba4"
     },
 
     {
@@ -43,7 +33,7 @@ function ChillHop() {
       id: uuidv4(),
       active: false,
       color: ["#146787", "#A56235"],
-      audio:"https://www.mboxdrive.com/Aaja%20We%20Mahiya%20(Imran%20Khan).mp3"
+      audio:"https://uploadbox.io/6a3v8zt78i36"
     },
 
     {
@@ -54,18 +44,7 @@ function ChillHop() {
       id: uuidv4(),
       active: false,
       color: ["#642F27", "#9CB4CE"],
-      audio:"https://www.mboxdrive.com/Justin%20Bieber%20-%20Peaches%20ft.%20Daniel%20Caesar,%20Giveon.mp3"
-    },
-
-    {
-      name: "Peaky Blinders",
-      artist: "Otnicka",
-      cover:
-        "https://preview.redd.it/lx9mgljetm881.png?width=640&crop=smart&auto=webp&s=33d823c66688bc3a254ac1efa1f5f1486745d6d4",
-      id: uuidv4(),
-      active: false,
-      color: ["#642F27", "#9CB4CE"],
-      audio:"https://www.mboxdrive.com/Otnicka%20-%20PEAKY%20BLINDERS.mp3"
+      audio:"https://uploadbox.io/bjg127m7cmlq"
     },
 
     {
